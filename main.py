1b8c399ca3edc6384e3d44d0ebdc8e58b97dc6db
Svetlana Zilpausha <70100791+SZilpausha@users.noreply.github.com>
2021-12-01 07:45:25 +0000

    Izlabota versija

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-#Proba
+def countNumbers(v): 
+ summa = 0
+ for simbols in v:
+    summa = summa + int(simbols)
+ return summa
+v = input("Ievadi skaitli: ")
+rez = countNumbers(v)
+print("Summa ir ", rez)
